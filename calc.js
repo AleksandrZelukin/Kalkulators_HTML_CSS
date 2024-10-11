097d38100d71c9b48950a6fc784ccb0f529d9ea7
Aleksandr <alexandr44@inbox.lv>
2024-10-11 19:09:12 +0300

    update apps

--- calc.js
+++ calc.js
@@ -3,13 +3,13 @@ let b = ''; //otrais skaitlis
 let sign = ''; //matemat operacija
 let finish = false;
 
-const digit = ['0','1','2','3','4','5','6','7','8','9','.'];
-const action = ['-','+','X','/'];
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
 
 
 const out = document.querySelector('.calc-screen p');
 
-function clearALL () {
+function clearAll () {
     a = '';
     b = '';
     sign = '';
@@ -17,17 +17,22 @@ function clearALL () {
     out.textContent = 0;
 }
 
-document.querySelector('.ac').oncick = clearALL;
+document.querySelector('.ac').onclick = clearAll;
 
-document.querySelector('.ac').oncick = (event) => {
+document.querySelector('.buttons').onclick = (event) => {
+    
     if(!event.target.classList.contains('btn')) return;
+    
     if(event.target.classList.contains('ac')) return;
 
     out.textContent = '';
+    
     const key = event.target.textContent;
 
+    
     if (digit.includes(key)) {
-        a+=key;
-        console.log(a, b, sign)
+        a += key;
+        console.log(a, b, sign);
+        out.textContent = a;
     }
 }
